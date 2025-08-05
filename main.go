96bd8690f2bae025ce7479537b85ca1fdba1f2b4
Eugene Martynov <tambov.j678@gmail.com>
2025-08-05 11:56:39 +0300

    implement usernames

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"time"
@@ -13,11 +14,16 @@ import (
 // Hub: maintains active clients and broadcasts messages to them.
 // ----------------------------------------------------------------------------
 
+type Message struct {
+	User string `json:"user"`
+	Text string `json:"text"`
+}
+
 type Hub struct {
 	// Registered connections.
-	clients map[*websocket.Conn]bool
+	clients map[*websocket.Conn]string
 	// Inbound messages from the clients.
-	broadcast chan []byte
+	broadcast chan Message
 	// Register requests from the clients.
 	register chan *websocket.Conn
 	// Unregister requests from clients.
@@ -26,8 +32,8 @@ type Hub struct {
 
 func newHub() *Hub {
 	return &Hub{
-		clients:    make(map[*websocket.Conn]bool),
-		broadcast:  make(chan []byte),
+		clients:    make(map[*websocket.Conn]string),
+		broadcast:  make(chan Message),
 		register:   make(chan *websocket.Conn),
 		unregister: make(chan *websocket.Conn),
 	}
@@ -37,7 +43,7 @@ func (h *Hub) run() {
 	for {
 		select {
 		case conn := <-h.register:
-			h.clients[conn] = true
+			h.clients[conn] = ""
 			log.Printf("client registered, total: %d", len(h.clients))
 
 		case conn := <-h.unregister:
@@ -48,9 +54,10 @@ func (h *Hub) run() {
 			}
 
 		case msg := <-h.broadcast:
+			b, _ := json.Marshal(msg)
 			for conn := range h.clients {
 				// send to each; if error, assume broken and unregister
-				if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+				if err := conn.WriteMessage(websocket.TextMessage, b); err != nil {
 					log.Printf("broadcast error: %v", err)
 					h.unregister <- conn
 				}
@@ -103,14 +110,30 @@ func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}()
+	_, userMsg, err := conn.ReadMessage()
+	if err != nil {
+		return
+	}
+	var userObj Message
+	if err := json.Unmarshal(userMsg, &userObj); err != nil || userObj.User == "" {
+		return
+	}
+	hub.clients[conn] = userObj.User
+	log.Printf("user %q joined", userObj.User)
 
 	// Read loop: on each message, push to hub.broadcast
 	for {
-		_, msg, err := conn.ReadMessage()
+		_, msgData, err := conn.ReadMessage()
 		if err != nil {
-			break
+			log.Printf("read error: %v", err)
+			continue
 		}
-		hub.broadcast <- msg
+		var msg Message
+		if err := json.Unmarshal(msgData, &msg); err == nil {
+			// Always use the username associated with the connection (not what client sends)
+			msg.User = hub.clients[conn]
+			hub.broadcast <- msg
+		}
 	}
 }
 
